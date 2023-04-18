9b9f927d682b44d3b30798abeef348add00e0e66
stanislavcubar <snorccrafter@gmail.com>
2023-04-18 10:42:38 +0700

    commit #1

--- summit_krsk/urls.py
+++ summit_krsk/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import order
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', order, name="Home")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
